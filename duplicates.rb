954adcead9ca7931c267aaa020be4dab79b35582
Magnus Wissler <magnus.wissler@softronic.se>
2011-11-23 14:37:11 +0100

    Added command line verbose option and ability to descend multiple directories.

--- duplicates.rb
+++ duplicates.rb
@@ -1,30 +1,37 @@
 #
-# Creates checksums for a set of files, then reports duplicates
+# Creates checksums for a set of files, then reports duplicates.
 #
+require 'optparse'
 require 'digest/md5'
 
 $files = Hash.new
 $files_processed = 0
+
 BLOCKSIZE = 8192
 
+# Arbitrary list of directories not to traverse.
 BLACKLIST = [ ".", "..", ".DS_Store", ".svn", ".metadata"]
+
+$options = {}
 
 #
 # Recursively descends a directory tree, computing the checksums of all files,
-# then storing duplicate checksums in a Hash.
+# then storing checksums in a Hash containing an array of names of the 
+# duplicate files.
 #
 def process_dir(dirname)
 	Dir.entries(dirname).each { |filename|
 		next if BLACKLIST.include? filename
 		entry = File.join dirname, filename
 		next if File.symlink? entry
+
 		if File.directory? entry then
 			process_dir entry
 		else
 			checksum = checksum entry
 			$files[checksum] ||= Array.new 
 			$files[checksum] << entry
-			printf "%s %s\n", checksum, entry if $verbose
+			printf "%s %s\n", checksum, entry if $options[:verbose]
 			$files_processed += 1
 		end
 	}
@@ -43,9 +50,29 @@ def checksum(filename)
 	}
 end
 
+cmd_opts = OptionParser.new do |opts|
+	opts.banner = "Usage duplicates.rb [-v] [dirname]"
 
+	$options[:verbose] = false
+	opts.on('-v', '--verbose', 'Be verbose. Reports the checksum and name of every file processed.') do
+		$options[:verbose] = true
+	end
 
-process_dir(".")
+	opts.on( '-h', '--help', 'Display this screen' ) do
+    	puts opts
+    	exit
+   end
+end
+
+cmd_opts.parse!
+
+if ARGV.size == 0 then
+	process_dir "." 
+else
+	ARGV.each do |dir|
+		process_dir(dir)
+	end
+end
 
 puts "#{$files_processed} files processed."
 puts "#{$files_processed - $files.size} duplicates found."
